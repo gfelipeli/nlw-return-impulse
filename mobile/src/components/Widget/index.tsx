0acc6b40bd34cc2c858fc63b7a594665484e5a60
gfelipeli <gfelipeli@protonmail.com>
2022-05-16 15:31:42 -0300

    form stage 4

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -6,6 +6,11 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
+import { Form } from '../Form';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -30,8 +35,13 @@ function Widget() {
       <BottomSheet
         ref={bottomSheetRef}
         snapPoints={[1, 280]}
+        backgroundStyle={styles.modal}
+        handleIndicatorStyle={styles.indicator}
       >
-
+        <Form 
+          feedbackType="BUG"
+          
+        />
       </BottomSheet>
     </>
   );
