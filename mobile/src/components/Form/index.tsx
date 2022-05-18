245454dac138de1cf04a1082f7b45781da022816
gfelipeli <gfelipeli@protonmail.com>
2022-05-18 00:03:27 -0300

    feedback functions

--- mobile/src/components/Form/index.tsx
+++ mobile/src/components/Form/index.tsx
@@ -1,67 +1,77 @@
-import { ArrowLeft } from 'phosphor-react-native';
-import React from 'react';
-import { 
-  View,
-  TextInput,
-  Image,
-  Text,
-  TouchableOpacity,
- } from 'react-native';
+import { ArrowLeft } from 'phosphor-react-native'
+import React, { useState } from 'react'
+import { View, TextInput, Image, Text, TouchableOpacity } from 'react-native'
 
-import { FeedbackType } from '../../components/Widget';
-import { Button } from '../../components/Button';
-import { ScreenshotButton } from '../../components/ScreenshotButton';
+import { captureScreen } from 'react-native-view-shot'
 
-import { theme } from '../../theme';
-import { styles } from './styles';
-import { feedbackTypes } from '../../utils/feedbackTypes';
+import { FeedbackType } from '../../components/Widget'
+import { Button } from '../../components/Button'
+import { ScreenshotButton } from '../../components/ScreenshotButton'
+
+import { theme } from '../../theme'
+import { styles } from './styles'
+import { feedbackTypes } from '../../utils/feedbackTypes'
 
 interface Props {
-  feedbackType: FeedbackType;
+  feedbackType: FeedbackType
+  onFeedbackCanceled: () => void
+  onFeedbackSent: () => void
 }
 
-export function Form({ feedbackType }: Props) {
-  const feedbackTypeInfo = feedbackTypes[feedbackType];
+export function Form({
+  feedbackType,
+  onFeedbackCanceled,
+  onFeedbackSent
+}: Props) {
+  const [screenshot, setScreenshot] = useState<string | null>(null)
+
+  const feedbackTypeInfo = feedbackTypes[feedbackType]
+
+  function handleScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8
+    })
+      .then(uri => setScreenshot(uri))
+      .catch(error => console.log(error))
+  }
+
+  function handleScreenshotRemove() {
+    setScreenshot(null)
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <TouchableOpacity>
-          <ArrowLeft 
+        <TouchableOpacity onPress={onFeedbackCanceled}>
+          <ArrowLeft
             size={24}
             weight="bold"
             color={theme.colors.text_secondary}
           />
         </TouchableOpacity>
         <View style={styles.titleContainer}>
-          <Image
-            source={feedbackTypeInfo.image}
-            style={styles.image}
-          />
-          <Text style={styles.titleText}>
-            {feedbackTypeInfo.title}
-          </Text>
-        </View> 
+          <Image source={feedbackTypeInfo.image} style={styles.image} />
+          <Text style={styles.titleText}>{feedbackTypeInfo.title}</Text>
+        </View>
       </View>
       <TextInput
         multiline
         style={styles.input}
         placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
         placeholderTextColor={theme.colors.text_secondary}
-
+        autoCorrect={false}
       />
 
       <View style={styles.footer}>
         <ScreenshotButton
-          onTakeShot={() => {}}
-          onRemoveShot={() => {}}
-          screenshot=""
+          onTakeShot={handleScreenshot}
+          onRemoveShot={handleScreenshotRemove}
+          screenshot={screenshot}
         />
 
-        <Button
-          isLoading={false}
-         />
+        <Button isLoading={false} />
       </View>
     </View>
-  );
+  )
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,37 +1,46 @@
-import React, { useRef } from 'react';
-import { TouchableOpacity } from 'react-native';
-import { ChatTeardropDots } from 'phosphor-react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import React, { useRef, useState } from 'react'
+import { TouchableOpacity } from 'react-native'
+import { ChatTeardropDots } from 'phosphor-react-native'
+import BottomSheet from '@gorhom/bottom-sheet'
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
-import { Options } from '../Options';
-import { Form } from '../Form';
-import { Success } from '../Success';
+import { Options } from '../Options'
+import { Form } from '../Form'
+import { Success } from '../Success'
 
-import { styles } from './styles';
-import { theme } from '../../theme';
-import { feedbackTypes } from '../../utils/feedbackTypes';
+import { styles } from './styles'
+import { theme } from '../../theme'
+import { feedbackTypes } from '../../utils/feedbackTypes'
 
-export type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes
 
 function Widget() {
-  const bottomSheetRef = useRef<BottomSheet>(null);
-  
-  function handleOpen(){
-    bottomSheetRef.current?.expand();
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+  const [feedbackSent, setFeedbackSent] = useState(false)
+
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen() {
+    bottomSheetRef.current?.expand()
+  }
+
+  function handleRestartFeedback() {
+    setFeedbackType(null)
+    setFeedbackSent(false)
+  }
+
+  function handleFeedbackSent() {
+    setFeedbackSent(true)
   }
 
   return (
     <>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={handleOpen}
-      >
-      <ChatTeardropDots
-        size={24}
-        weight="bold"
-        color={theme.colors.text_on_brand_color}
-      />
+      <TouchableOpacity style={styles.button} onPress={handleOpen}>
+        <ChatTeardropDots
+          size={24}
+          weight="bold"
+          color={theme.colors.text_on_brand_color}
+        />
       </TouchableOpacity>
 
       <BottomSheet
@@ -40,11 +49,24 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-        <Success
-        />
+        {feedbackSent ? (
+          <Success />
+        ) : (
+          <>
+            {feedbackType ? (
+              <Form
+                feedbackType={feedbackType}
+                onFeedbackSent={handleFeedbackSent}
+                onFeedbackCanceled={handleRestartFeedback}
+              />
+            ) : (
+              <Options onFeedbackTypeChanged={setFeedbackType} />
+            )}
+          </>
+        )}
       </BottomSheet>
     </>
-  );
+  )
 }
 
-export default gestureHandlerRootHOC(Widget);
+export default gestureHandlerRootHOC(Widget)
