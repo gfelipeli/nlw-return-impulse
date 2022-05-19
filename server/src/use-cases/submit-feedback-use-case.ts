6818e4f76a2f05826b5924a7b42617a0b071b374
gfelipeli <gfelipeli@protonmail.com>
2022-05-19 01:37:10 -0300

    feat: back and front-end integrated

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -1,29 +1,29 @@
-import { MailAdapter } from "../adapters/mail-adapter";
-import { FeedbacksRepository } from "../repositories/feedbacks-repository";
+import { MailAdapter } from '../adapters/mail-adapter'
+import { FeedbacksRepository } from '../repositories/feedbacks-repository'
 
 interface SubmitFeedbackUseCaseRequest {
-  type: string;
-  comment: string;
-  screenshot?: string;
+  type: string
+  comment: string
+  screenshot?: string
 }
 
 export class SubmitFeedbackUseCase {
   constructor(
     private feedbacksRepository: FeedbacksRepository,
-    private mailAdapter: MailAdapter,
+    private mailAdapter: MailAdapter
   ) {}
-  
-  async execute(request: SubmitFeedbackUseCaseRequest ) {
-    const { type, comment, screenshot } = request;
-    
+
+  async execute(request: SubmitFeedbackUseCaseRequest) {
+    const { type, comment, screenshot } = request
+
     if (!type) {
-      throw new Error('Type is required.');
+      throw new Error('Type is required.')
     }
 
     if (!comment) {
-      throw new Error('Comment is required.');
+      throw new Error('Comment is required.')
     }
-    
+
     if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
       throw new Error('Invalid screenshot format.')
     }
@@ -31,17 +31,18 @@ export class SubmitFeedbackUseCase {
     await this.feedbacksRepository.create({
       type,
       comment,
-      screenshot,
+      screenshot
     })
 
     await this.mailAdapter.sendMail({
       subject: 'New feedback',
       body: [
-      `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-      `<p>Feedback Type: ${type}</p>`,
-      `<p>Comment: ${comment}</p>`,
-      `</div>` 
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+        `<p>Feedback Type: ${type}</p>`,
+        `<p>Comment: ${comment}</p>`,
+        screenshot ? `<img src="${screenshot}" />` : ``,
+        `</div>`
       ].join('\n')
     })
-  } 
+  }
 }
